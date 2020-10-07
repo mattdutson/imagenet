f9f3829291491b8cbbfb542d989bf9baacb3137e
Matthew Dutson <mattdutson@icloud.com>
2020-10-07 10:31:37 -0500

    [S] Style changes to dataset.py

--- mobilenet/dataset.py
+++ mobilenet/dataset.py
@@ -27,30 +27,30 @@ def imagenet(split, size=(320, 320), augment=False):
         of batches in the split.
     """
 
-    def _map(item):
+    def preprocess(item):
         # float32 is better behaved during augmentation
-        image = tf.image.convert_image_dtype(item['image'], tf.float32)
+        x = tf.image.convert_image_dtype(item['image'], tf.float32)
 
         if augment:
-            image = tf.image.random_flip_left_right(image)
-            image = tf.image.random_brightness(image, BRIGHTNESS_DELTA)
-            image = tf.image.random_contrast(image, CONTRAST_MIN, CONTRAST_MAX)
-            image = tf.image.random_hue(image, HUE_DELTA)
-            image = tf.image.random_saturation(image, SATURATION_MIN, SATURATION_MAX)
+            x = tf.image.random_flip_left_right(x)
+            x = tf.image.random_brightness(x, BRIGHTNESS_DELTA)
+            x = tf.image.random_contrast(x, CONTRAST_MIN, CONTRAST_MAX)
+            x = tf.image.random_hue(x, HUE_DELTA)
+            x = tf.image.random_saturation(x, SATURATION_MIN, SATURATION_MAX)
 
             # Brightness and contrast shifts may put pixel values out of
             # the range [0, 1]
-            image = tf.clip_by_value(image, 0.0, 1.0)
+            x = tf.clip_by_value(x, 0.0, 1.0)
 
         # Pad after augmentation so zero padded pixels aren't changed
-        image = tf.image.resize_with_pad(image, size[0], size[1])
-        label = tf.one_hot(item['label'], N_CLASSES)
-        return image, label
+        x = tf.image.resize_with_pad(x, size[0], size[1])
+        y = tf.one_hot(item['label'], N_CLASSES)
+        return x, y
 
-    def _prepare(dataset):
+    def prepare(dataset):
         return (
             dataset
-                .map(_map)
+                .map(preprocess)
                 .repeat()
                 .prefetch(N_PREFETCH)
                 .batch(BATCH_SIZE))
@@ -59,13 +59,13 @@ def imagenet(split, size=(320, 320), augment=False):
         full, info = tfds.load('imagenet2012', split='train', with_info=True)
         n_train = info.splits['train'].num_examples - N_VAL
         if split == 'train':
-            return _prepare(full.take(n_train)), _n_batches(n_train)
+            return prepare(full.take(n_train)), _n_batches(n_train)
         else:
-            return _prepare(full.skip(n_train)), _n_batches(N_VAL)
+            return prepare(full.skip(n_train)), _n_batches(N_VAL)
     elif split == 'test':
         test, info = tfds.load('imagenet2012', split='val', with_info=True)
         n_test = info.splits['val'].num_examples
-        return _prepare(test), _n_batches(n_test)
+        return prepare(test), _n_batches(n_test)
     else:
         raise ValueError('split must be "train", "val", or "test"')
 
